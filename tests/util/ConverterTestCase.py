fbfc34f0acd9a0d242b49fbb8010974a6f021b0b
Yutaka Yoshisaka <yyoshisaka@nvidia.com>
2025-12-12 09:39:51 +0900

    usd-exchange 2.2.0.rc2

--- tests/util/ConverterTestCase.py
+++ tests/util/ConverterTestCase.py
@@ -1,6 +1,5 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 The Newton Developers
 # SPDX-License-Identifier: Apache-2.0
-import typing
 
 import omni.asset_validator
 import usdex.test
@@ -10,14 +9,6 @@ from pxr import UsdGeom
 class ConverterTestCase(usdex.test.TestCase):
 
     defaultUpAxis = UsdGeom.Tokens.z  # noqa: N815
-
-    defaultValidationIssuePredicates: typing.ClassVar[list[omni.asset_validator.IssuePredicates]] = [  # noqa: N815
-        # URDF USD Converter uses nested bodies, as it more faithfully matches the kinematic tree in URDF.
-        #
-        # Once adopted, the asset validator should be updated to support nested bodies within articulations. For now, we just ignore the issues.
-        omni.asset_validator.IssuePredicates.ContainsMessage("Enabled rigid body is missing xformstack reset, when a child of a rigid body"),
-        omni.asset_validator.IssuePredicates.ContainsMessage("ArticulationRootAPI definition on a kinematic rigid body is not allowed"),
-    ]
 
     def setUp(self):
         super().setUp()
